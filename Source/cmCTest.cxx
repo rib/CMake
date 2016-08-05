37507ffb11c441380ce6d6635b3992e09937f090
Chuck Atkins <chuck.atkins@kitware.com>
2016-08-05 14:11:46 -0400

    Add additional <= and >= comparison operators
    This adds the LESS_EQUAL, GREATER_EQUAL, and associated STR and VERSION_
    equivalents to use the combined <= and >= functionality.

--- Source/cmCTest.cxx
+++ Source/cmCTest.cxx
@@ -367,10 +367,8 @@ bool cmCTest::ShouldCompressTestOutput()
     std::string cdashVersion = this->GetCDashVersion();
     // version >= 1.6?
     bool cdashSupportsGzip =
-      cmSystemTools::VersionCompare(cmSystemTools::OP_GREATER,
-                                    cdashVersion.c_str(), "1.6") ||
-      cmSystemTools::VersionCompare(cmSystemTools::OP_EQUAL,
-                                    cdashVersion.c_str(), "1.6");
+      cmSystemTools::VersionCompare(cmSystemTools::OP_GREATER_EQUAL,
+        cdashVersion.c_str(), "1.6");
     this->CompressTestOutput &= cdashSupportsGzip;
     this->ComputedCompressTestOutput = true;
   }
